39b86b02617a2ad0936f6ad43fc3c85e323041bd
Daisuke Ikeda <ohayou.ikechan@gmail.com>
2016-03-25 17:05:06 +0900

    first commit

---
+++ hello_world.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: remove amazon linker
+Plugin URI: 
+Description: (プラグインの短い説明)
+Version: (プラグインのバージョン番号。例: 1.0)
+Author: (プラグイン作者の名前)
+Author URI: (プラグイン作者の URI)
+License: (ライセンス名の「スラッグ」 例: GPL2)
+*/
+?>
